523d346e688855633da4590eaba704d6ff16aaf2
Fernando Serapio <fhernandosc@gmail.com>
2013-10-11 17:30:26 -0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Vocales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblLetraMayor = new System.Windows.Forms.Label();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnI = new System.Windows.Forms.Button();
+            this.btnO = new System.Windows.Forms.Button();
+            this.btnU = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.txtI = new System.Windows.Forms.TextBox();
+            this.txtO = new System.Windows.Forms.TextBox();
+            this.txtU = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtU);
+            this.groupBox1.Controls.Add(this.txtO);
+            this.groupBox1.Controls.Add(this.txtI);
+            this.groupBox1.Controls.Add(this.txtE);
+            this.groupBox1.Controls.Add(this.txtA);
+            this.groupBox1.Controls.Add(this.btnU);
+            this.groupBox1.Controls.Add(this.btnO);
+            this.groupBox1.Controls.Add(this.btnI);
+            this.groupBox1.Controls.Add(this.btnE);
+            this.groupBox1.Controls.Add(this.btnA);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 225);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccion";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblLetraMayor
+            // 
+            this.lblLetraMayor.AutoSize = true;
+            this.lblLetraMayor.Location = new System.Drawing.Point(12, 240);
+            this.lblLetraMayor.Name = "lblLetraMayor";
+            this.lblLetraMayor.Size = new System.Drawing.Size(35, 13);
+            this.lblLetraMayor.TabIndex = 0;
+            this.lblLetraMayor.Text = "label1";
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(6, 43);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(75, 23);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnE
+            // 
+            this.btnE.Location = new System.Drawing.Point(6, 72);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(75, 23);
+            this.btnE.TabIndex = 2;
+            this.btnE.Text = "E";
+            this.btnE.UseVisualStyleBackColor = true;
+            this.btnE.Click += new System.EventHandler(this.btnE_Click);
+            // 
+            // btnI
+            // 
+            this.btnI.Location = new System.Drawing.Point(6, 101);
+            this.btnI.Name = "btnI";
+            this.btnI.Size = new System.Drawing.Size(75, 23);
+            this.btnI.TabIndex = 3;
+            this.btnI.Text = "I";
+            this.btnI.UseVisualStyleBackColor = true;
+            this.btnI.Click += new System.EventHandler(this.btnI_Click);
+            // 
+            // btnO
+            // 
+            this.btnO.Location = new System.Drawing.Point(6, 130);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(75, 23);
+            this.btnO.TabIndex = 4;
+            this.btnO.Text = "O";
+            this.btnO.UseVisualStyleBackColor = true;
+            this.btnO.Click += new System.EventHandler(this.btnO_Click);
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(6, 159);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(75, 23);
+            this.btnU.TabIndex = 5;
+            this.btnU.Text = "U";
+            this.btnU.UseVisualStyleBackColor = true;
+            this.btnU.Click += new System.EventHandler(this.btnU_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Enabled = false;
+            this.txtA.Location = new System.Drawing.Point(87, 45);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(86, 20);
+            this.txtA.TabIndex = 6;
+            // 
+            // txtE
+            // 
+            this.txtE.Enabled = false;
+            this.txtE.Location = new System.Drawing.Point(87, 74);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(86, 20);
+            this.txtE.TabIndex = 7;
+            // 
+            // txtI
+            // 
+            this.txtI.Enabled = false;
+            this.txtI.Location = new System.Drawing.Point(87, 103);
+            this.txtI.Name = "txtI";
+            this.txtI.Size = new System.Drawing.Size(86, 20);
+            this.txtI.TabIndex = 8;
+            // 
+            // txtO
+            // 
+            this.txtO.Enabled = false;
+            this.txtO.Location = new System.Drawing.Point(87, 132);
+            this.txtO.Name = "txtO";
+            this.txtO.Size = new System.Drawing.Size(86, 20);
+            this.txtO.TabIndex = 9;
+            // 
+            // txtU
+            // 
+            this.txtU.Enabled = false;
+            this.txtU.Location = new System.Drawing.Point(87, 161);
+            this.txtU.Name = "txtU";
+            this.txtU.Size = new System.Drawing.Size(86, 20);
+            this.txtU.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblLetraMayor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtU;
+        private System.Windows.Forms.TextBox txtO;
+        private System.Windows.Forms.TextBox txtI;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnU;
+        private System.Windows.Forms.Button btnO;
+        private System.Windows.Forms.Button btnI;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblLetraMayor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vocales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtA.Text = txtE.Text = txtI.Text = txtO.Text = txtU.Text = "0";
+            lblLetraMayor.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtA.Text = txtE.Text = txtI.Text = txtO.Text = txtU.Text = "0";
+            lblLetraMayor.Text = "";
+        }
+        
+        private void ActualizarResultado(TextBox objeto)
+        {
+            objeto.Text = (Convert.ToInt32(objeto.Text) + 1).ToString();
+
+            Dictionary<TextBox, int> diccionario = new Dictionary<TextBox, int>();
+            diccionario.Add(txtA, Convert.ToInt32(objeto.Text));
+            diccionario.Add(txtE, Convert.ToInt32(objeto.Text));
+            diccionario.Add(txtI, Convert.ToInt32(objeto.Text));
+            diccionario.Add(txtO, Convert.ToInt32(objeto.Text));
+            diccionario.Add(txtU, Convert.ToInt32(objeto.Text));
+
+            var item = diccionario.Keys.OrderByDescending(t => Convert.ToInt32(t.Text)).First();
+
+            lblLetraMayor.Text = string.Format("La mas alta es {0}", item.Name.Substring(3));
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado(txtA);
+        }
+        
+        private void btnE_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado(txtE);
+        }
+
+        private void btnI_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado(txtI);
+        }
+
+        private void btnO_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado(txtO);
+        }
+
+        private void btnU_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado(txtU);
+        }
+    }
+}
